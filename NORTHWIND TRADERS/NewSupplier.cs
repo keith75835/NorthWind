9e3c04659d2fad3a3bce8ef3919d278a3e997bcb
keith75835 <51826465+keith75835@users.noreply.github.com>
2019-07-09 08:59:55 -0500

    Add files via upload

---
+++ NORTHWIND TRADERS/NewSupplier.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NORTHWIND_TRADERS
+{
+    class NewSupplier
+    {
+    }
+}
---
+++ NORTHWIND TRADERS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace NORTHWIND_TRADERS
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("New Supplier");
+            string newSupplier = Console.ReadLine();
+            string query = "INSERT INTO Suppliers ";
+
+        }
+        public void viewsuppliers()
+        {
+            using (SqlConnection connection = new SqlConnection(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = NORTHWND; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Suppliers", connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                Console.WriteLine(reader.GetValue(i));
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
